f852534485d60fc822a767907d7a5e9d5dae5b8c
HinnyTsang <mhtsangab@connect.ust.hk>
2022-11-17 01:21:54 +0800

    cs144: Update warm up webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,8 +1,10 @@
 #include "socket.hh"
 #include "util.hh"
 
+#include <address.hh>
 #include <cstdlib>
 #include <iostream>
+#include <socket.hh>
 
 using namespace std;
 
@@ -17,8 +19,15 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    TCPSocket sock;
+
+    sock.connect(Address(host, "http"));
+    sock.write("GET " + path + " HTTP/1.1\r\nHOST: " + host + "\r\n\r\n");
+    while (!sock.eof()) {
+        cout << sock.read();
+    }
+    sock.close();
+    return;
 }
 
 int main(int argc, char *argv[]) {
